Fix variance bug in EM.

phasefinder/models.py
@@ -54,7 +54,7 @@ class MRA(nn.Module):
 				for _ in range(iters):
 					weights = torch.exp( torch.einsum("gij,j->gi", GX, mean)/variance )
 					mean_new = ( (GX*weights.unsqueeze(-1)).sum(0)/weights.unsqueeze(-1).sum(0) ).mean(0)
-					variance = mean.pow(2).sum() + X.pow(2).sum()/X.size(0) - 2*mean.dot(mean_new)
+					variance = ( mean.pow(2).sum() + X.pow(2).sum()/X.size(0) - 2*mean.dot(mean_new) )/self.input_dim
 					mean = mean_new
 				self.mean.copy_(mean)
 				self.variance_unconstrained.copy_(inverse_softplus(variance))

phasefinder2/models.py
@@ -43,7 +43,7 @@ class GMM(nn.Module):
 		with torch.no_grad():
 			weights = torch.exp( torch.einsum("gij,j->gi", self.GX, self.mean)/self.variance )
 			mean_new = ( (self.GX*weights.unsqueeze(-1)).sum(0)/weights.unsqueeze(-1).sum(0) ).mean(0)
-			self.variance = self.mean.pow(2).sum() + self.second_moment - 2*self.mean.dot(mean_new)
+			self.variance = ( self.second_moment - 2*self.mean.dot(mean_new) + self.mean.pow(2).sum() )/self.input_dim
 			self.mean = mean_new
 
 	def loss(self):