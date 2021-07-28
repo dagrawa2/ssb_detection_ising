6cc50dd0296e691778e7c1eee2d4e749230e2b24
dagrawa2 <dagrawa2@vols.utk.edu>
2021-07-27 20:21:57 -0400

    Refactor plotting script and fix heatmaps.

--- traditional.py
+++ traditional.py
@@ -10,42 +10,60 @@ matplotlib.rc("ytick", labelsize=14)
 from phasefinder.datasets import Ising
 
 
-def plot(results_dir):
-	bins = 50
+def gather_Ms(data_dir, L):
 	temperatures = []
+	measurements = []
+	L_dir = os.path.join(data_dir, "L{:d}".format(L))
+	for temperature_dir in sorted(os.listdir(L_dir)):
+		I = Ising()
+		Ms = I.load_M(os.path.join(L_dir, temperature_dir), per_spin=True)
+		temperatures.append(I.T)
+		measurements.append(Ms)
+	temperatures = np.array(temperatures)
+	measurements = np.stack(measurements, 0)
+	output_dir = "results/magnetization/L{:d}".format(L)
+	os.makedirs(output_dir, exist_ok=True)
+	np.savez(os.path.join(output_dir, "measurements.npz"), temperatures=temperatures, measurements=measurements)
+
+def calculate_stats(results_dir, observable_name, L, bins=50):
+	data = np.load(os.path.join(results_dir, observable_name, "L{:d}".format(L), "measurements.npz"))
+	temperatures = data["temperatures"]
+	measurements = data["measurements"]
 	distributions = []
 	order_means, order_stds = [], []
 	binder_means, binder_stds = [], []
-	for temperature_dir in sorted(os.listdir(results_dir)):
-		I = Ising()
-		Ms = I.load_M(os.path.join(results_dir, temperature_dir), per_spin=True)
-		hist, bin_edges = np.histogram(Ms, bins=bins, range=(-1, 1), density=False)
-		hist = hist/len(Ms)
+	for i in range(measurements.shape[0]):
+		hist, bin_edges = np.histogram(measurements[i], bins=bins, range=(-1, 1), density=False)
+		hist = hist/measurements.shape[1]
 		distributions.append(hist)
-		order_mean, order_std = I.jackknife(Ms, lambda x: np.mean(np.abs(x)))
+		order_mean, order_std = Ising().jackknife(measurements[i], lambda x: np.mean(np.abs(x)))
 		order_means.append(order_mean)
 		order_stds.append(order_std)
-		binder_mean, binder_std = I.jackknife(Ms, lambda x: 1 - np.mean(x**4)/(3*np.mean(x**2)**2))
+		binder_mean, binder_std = Ising().jackknife(measurements[i], lambda x: 1 - np.mean(x**4)/(3*np.mean(x**2)**2))
 		binder_means.append(binder_mean)
 		binder_stds.append(binder_std)
-		temperatures.append(I.T)
-	L = I.L
-	temperatures = np.array(temperatures)
-	distributions = np.stack(distributions, axis=1)
+	distributions = np.stack(distributions, 0)
 	order_means = np.array(order_means)
 	order_stds = np.array(order_stds)
 	binder_means = np.array(binder_means)
 	binder_stds = np.array(binder_stds)
+	np.savez(os.path.join(results_dir, observable_name, "L{:d}".format(L), "stats.npz"), distributions=distributions, order_means=order_means, order_stds=order_stds, binder_means=binder_means, binder_stds=binder_stds)
+
+def plot(results_dir, observable_name, L):
+	temperatures = np.load(os.path.join(results_dir, observable_name, "L{:d}".format(L), "measurements.npz"))["temperatures"]
+	stats = np.load(os.path.join(results_dir, observable_name, "L{:d}".format(L), "stats.npz"))
+	distributions = stats["distributions"]
+	order_means, order_stds = stats["order_means"], stats["order_stds"]
+	binder_means, binder_stds = stats["binder_means"], stats["binder_stds"]
 	# distributions
 	plt.figure()
-	plt.imshow(distributions, cmap="gray", vmin=0, vmax=1)
+	plt.imshow(np.flip(distributions.T, 0), cmap="gray_r", vmin=0, vmax=1, extent=(temperatures.min(), temperatures.max(), -1, 1), aspect="auto")
 	plt.axvline(x=2/np.log(1+np.sqrt(2)), color="blue", linestyle="dashed")
-	plt.yticks(np.flip(np.arange(bins+1)-0.5), bin_edges)
 	plt.xlabel(r"$T$", fontsize=12)
 	plt.ylabel(r"$M$", fontsize=12)
 	plt.title(r"$L="+str(int(L))+r"$", fontsize=16)
 	plt.tight_layout()
-	plots_dir = "plots/L{:d}".format(L)
+	plots_dir = os.path.join(results_dir, "plots", observable_name, "L{:d}".format(L))
 	os.makedirs(plots_dir, exist_ok=True)
 	plt.savefig(os.path.join(plots_dir, "distribution.png"))
 	plt.close()
@@ -82,7 +100,11 @@ if __name__ == "__main__":
 	Ls = [16, 32, 64, 128]
 
 	for L in Ls:
+#		print("Gathering magnetizations for L={:d} . . . ".format(L))
+#		gather_Ms("data", L)
+#		print("Calculating stats for L={:d} . . . ".format(L))
+#		calculate_stats("results", "magnetization", L)
 		print("Plotting L={:d} . . . ".format(L))
-		plot("data/L{:d}".format(L))
+		plot("results", "magnetization", L)
 
 	print("Done!")
