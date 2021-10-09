2284f3ca08045a492c63085f3ea28bdfec2596c1
dagrawa2 <dagrawa2@vols.utk.edu>
2021-10-08 20:53:54 -0400

    Add plots of stats and times.

---
+++ plots.py
@@ -0,0 +1,81 @@
+import os
+import json
+import numpy as np
+
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+matplotlib.rc("xtick", labelsize=14)
+matplotlib.rc("ytick", labelsize=14)
+
+def plot_stats(results_dir, J, observable_name, L, N=None):
+	if N is not None:
+		stats = dict(np.load(os.path.join(results_dir, "processed", J, observable_name, "L{:d}".format(L), "N{:d}".format(N), "stats.npz")))
+		output_dir = os.path.join(results_dir, "plots", J, observable_name, "L{:d}".format(L), "N{:d}".format(N))
+	else:
+		stats = dict(np.load(os.path.join(results_dir, "processed", J, observable_name, "L{:d}".format(L), "stats.npz")))
+		output_dir = os.path.join(results_dir, "plots", J, observable_name, "L{:d}".format(L))
+	os.makedirs(output_dir, exist_ok=True)
+	stats["distribution_range"] = stats["distribution_range"].tolist() if "distribution_range" in stats else [-1, 1]
+	# distributions
+	plt.figure()
+	plt.imshow(np.flip(stats["distributions"].T, 0), cmap="gray_r", vmin=0, vmax=1, extent=(stats["temperatures"].min(), stats["temperatures"].max(), *stats["distribution_range"]), aspect="auto")
+	plt.axvline(x=2/np.log(1+np.sqrt(2)), color="blue", linestyle="dashed")
+	plt.xlabel(r"$T$", fontsize=12)
+	plt.tight_layout()
+	plt.savefig(os.path.join(output_dir, "distribution.png"))
+	plt.close()
+	# order parameter
+	plt.figure()
+	plt.plot(stats["temperatures"], stats["order_means"], color="black")
+	plt.plot(stats["temperatures"], stats["order_means"]-stats["order_stds"], color="black", linestyle="dashed")
+	plt.plot(stats["temperatures"], stats["order_means"]+stats["order_stds"], color="black", linestyle="dashed")
+	plt.axvline(x=2/np.log(1+np.sqrt(2)), color="blue", linestyle="dashed")
+	plt.xlabel(r"$T$", fontsize=12)
+	plt.tight_layout()
+	plt.savefig(os.path.join(output_dir, "order.png"))
+	plt.close()
+	# U_4 Binder cumulant
+	plt.figure()
+	plt.plot(stats["temperatures"], stats["u4_means"], color="black")
+	plt.plot(stats["temperatures"], stats["u4_means"]-stats["u4_stds"], color="black", linestyle="dashed")
+	plt.plot(stats["temperatures"], stats["u4_means"]+stats["u4_stds"], color="black", linestyle="dashed")
+	plt.axvline(x=2/np.log(1+np.sqrt(2)), color="blue", linestyle="dashed")
+	plt.xlabel(r"$T$", fontsize=12)
+	plt.tight_layout()
+	plt.savefig(os.path.join(output_dir, "u4.png"))
+	plt.close()
+
+
+def plot_times(results_dir):
+	output_dir = os.path.join(results_dir, "plots")
+	os.makedirs(output_dir, exist_ok=True)
+	times = np.load(os.path.join(results_dir, "processed", "times.npz"))
+	x = np.arange(len(times["Ls"]))
+	width = 0.35
+	plt.figure()
+	plt.bar(x-width/2, times["AE_means"], width, yerr=times["AE_stds"], color="red", label="Baseline encoder")
+	plt.bar(x+width/2, times["GE_means"], width, yerr=times["GE_stds"], color="blue", label="GE-encoder")
+	plt.bar(x+width/2, times["preprocessing_means"], width, yerr=times["preprocessing_stds"], bottom=times["GE_means"], color="lightblue", label="Checkerboard averaging")
+	plt.legend(loc="upper left", bbox_to_anchor=(0, 1), fancybox=True, fontsize=10)
+	plt.xlabel("Lattice size (L)", fontsize=12)
+	plt.xticks(x, times["Ls"].astype(np.int32))
+	plt.ylabel("Time (min)", fontsize=12)
+	plt.tight_layout()
+	plt.savefig(os.path.join(output_dir, "times.png"))
+	plt.close()
+
+
+if __name__ == "__main__":
+	Js = ["ferromagnetic", "antiferromagnetic"]
+
+	print("Plotting statistics . . . ")
+	for J in Js:
+		plot_stats("results", J, "magnetization", 128)
+		plot_stats("results", J, "latent", 128, N=2048)
+		plot_stats("results", J, "latent_equivariant", 128, N=2048)
+
+	print("Plotting times . . . ")
+	plot_times("results")
+
+	print("Done!")
---
+++ process.py
@@ -0,0 +1,196 @@
+import os
+import json
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+from phasefinder import jackknife
+from phasefinder.datasets import Ising
+
+
+###process magnetization, order parameter curves, and U_4 Binder cumulant curves
+
+def gather_magnetizations(data_dir, results_dir, J, L):
+	temperatures = []
+	measurements = []
+	L_dir = os.path.join(data_dir, J, "L{:d}".format(L))
+	for temperature_dir in sorted(os.listdir(L_dir)):
+		I = Ising()
+		Ms = I.magnetization(os.path.join(L_dir, temperature_dir), per_spin=True, staggered=(J=="antiferromagnetic"))
+		temperatures.append(I.T)
+		measurements.append(Ms)
+	temperatures = np.array(temperatures)
+	measurements = np.stack(measurements, 0)
+	output_dir = "{}/{}/magnetization/L{:d}".format(results_dir, J, L)
+	os.makedirs(output_dir, exist_ok=True)
+	np.savez(os.path.join(output_dir, "measurements.npz"), temperatures=temperatures, measurements=measurements)
+
+
+def calculate_stats(results_dir, J, observable_name, L, N=None, bins=50):
+	if N is not None:
+		measurements = np.load(os.path.join(results_dir, J, observable_name, "L{:d}".format(L), "N{:d}".format(N), "measurements.npz"))
+	else:
+		measurements = np.load(os.path.join(results_dir, J, observable_name, "L{:d}".format(L), "measurements.npz"))
+	temperatures = measurements["temperatures"]
+	measurements = measurements["measurements"].T
+	order_means, order_stds = jackknife.calculate_mean_std( jackknife.calculate_samples(np.abs(measurements)) )
+	u4_means, u4_stds = jackknife.calculate_mean_std( \
+		1 - jackknife.calculate_samples(measurements**4)/( 3*jackknife.calculate_samples(measurements**2)**2 ) )
+	distribution_range = (measurements.min(), measurements.max())
+	distributions = []
+	for slice in measurements.T:
+		hist, _ = np.histogram(slice, bins=bins, range=distribution_range, density=False)
+		hist = hist/len(slice)
+		distributions.append(hist)
+	distributions = np.stack(distributions, 0)
+	distribution_range = np.array(list(distribution_range))
+	if N is not None:
+		output_dir = os.path.join(results_dir, "processed", J, observable_name, "L{:d}".format(L), "N{:d}".format(N))
+	else:
+		output_dir = os.path.join(results_dir, "processed", J, observable_name, "L{:d}".format(L))
+	os.makedirs(output_dir, exist_ok=True)
+	np.savez(os.path.join(output_dir, "stats.npz"), temperatures=temperatures, distributions=distributions, distribution_range=distribution_range, order_means=order_means, order_stds=order_stds, u4_means=u4_means, u4_stds=u4_stds)
+
+
+### process critical temperature estimates
+
+def U4_samples(results_dir, J, observable_name, L, N=None):
+	if N is not None:
+		measurements = np.load(os.path.join(results_dir, J, observable_name, "L{:d}".format(L), "N{:d}".format(N), "measurements.npz"))["measurements"].T
+	else:
+		measurements = np.load(os.path.join(results_dir, J, observable_name, "L{:d}".format(L), "measurements.npz"))["measurements"].T
+	samples_2 = jackknife.calculate_samples(measurements**2)
+	samples_4 = jackknife.calculate_samples(measurements**4)
+	samples = 1 - samples_4/(3*samples_2**2)
+	return samples
+
+
+def critical_temperature_samples(temperatures, u4samples):
+	tc_samples = []
+	for samples in u4samples:
+		cs = CubicSpline(temperatures, samples)
+		roots = cs.derivative(2).roots(extrapolate=False)
+		values = np.abs(cs(roots, nu=1))
+		tc_samples.append( roots[np.argmax(values)] )
+	tc_samples = np.array(tc_samples)
+	return tc_samples
+
+
+def calculate_critical_temperatures(results_dir, J, Ls, Ns, temperature_range=None):
+	output_dict = {"L": Ls, "M_mean": [], "M_std": [], "N": Ns}
+	for L in Ls:
+		for model in ["AE", "GE"]:
+			for stat in ["mean", "std"]:
+				output_dict["L{:d}_{}_{}".format(L, model, stat)] = []
+	for L in Ls:
+		temperatures = np.load(os.path.join(results_dir, J, "magnetization", "L{:d}".format(L), "measurements.npz"))["temperatures"]
+		u4samples = U4_samples(results_dir, J, "magnetization", L)
+		if temperature_range is not None:
+			T_within_range_indices = np.where(np.logical_and(temperatures>=temperature_range[0], temperatures<=temperature_range[1]))[0]
+			T_min_index, T_max_index = T_within_range_indices.min(), T_within_range_indices.max()
+			temperatures = temperatures[T_min_index:T_max_index+1]
+			u4samples = u4samples[:,T_min_index:T_max_index+1]
+		tc_samples = critical_temperature_samples(temperatures, u4samples)
+		tc_mean, tc_std = jackknife.calculate_mean_std(tc_samples)
+		output_dict["M_mean"].append(tc_mean)
+		output_dict["M_std"].append(tc_std)
+		for N in Ns:
+			for (model, observable_name) in [("AE", "latent"), ("GE", "latent_equivariant")]:
+				temperatures = np.load(os.path.join(results_dir, J, observable_name, "L{:d}".format(L), "N{:d}".format(N), "measurements.npz"))["temperatures"]
+				u4samples = U4_samples(results_dir, J, observable_name, L, N)
+				if temperature_range is not None:
+					T_within_range_indices = np.where(np.logical_and(temperatures>=temperature_range[0], temperatures<=temperature_range[1]))[0]
+					T_min_index, T_max_index = T_within_range_indices.min(), T_within_range_indices.max()
+					temperatures = temperatures[T_min_index:T_max_index+1]
+					u4samples = u4samples[:,T_min_index:T_max_index+1]
+				tc_samples = critical_temperature_samples(temperatures, u4samples)
+				tc_mean, tc_std = jackknife.calculate_mean_std(tc_samples)
+				output_dict["L{:d}_{}_mean".format(L, model)].append(tc_mean)
+				output_dict["L{:d}_{}_std".format(L, model)].append(tc_std)
+	output_dict = {key: np.asarray(value) for (key, value) in output_dict.items()}
+	output_dir = os.path.join(results_dir, "processed", J)
+	os.makedirs(output_dir, exist_ok=True)
+	np.savez(os.path.join(output_dir, "tc.npz"), **output_dict)
+
+
+### process execution times
+
+def calculate_times(results_dir, Js, Ls, Ns):
+	output_dict = {"Ls": Ls, "preprocessing_means": [], "preprocessing_stds": []}
+	for model in ["AE", "GE"]:
+		for stat in ["mean", "std"]:
+			output_dict["{}_{}s".format(model, stat)] = []
+	for L in Ls:
+		times = []
+		for J in Js:
+			times_J = []
+			for dir in sorted(os.listdir(os.path.join("data", J, "L{:d}".format(L)))):
+				with open(os.path.join("data", J, "L{:d}".format(L), dir, "time_symmetric.txt"), "r") as f:
+					times_J.append( float(f.read()) )
+			times.append(times_J)
+		times = np.array(times).sum(1)
+		output_dict["preprocessing_means"].append( times.mean() )
+		output_dict["preprocessing_stds"].append( times.std() )
+	for L in Ls:
+		for (model, observable_name) in [("AE", "latent"), ("GE", "latent_equivariant")]:
+			times = []
+			for J in Js:
+				for N in Ns:
+					with open(os.path.join(results_dir, J, observable_name, "L{:d}".format(L), "N{:d}".format(N), "results.json"), "r") as f:
+						times.append( json.load(f)["time"] )
+			times = np.array(times)
+			output_dict["{}_means".format(model)].append( times.mean() )
+			output_dict["{}_stds".format(model)].append( times.std() )
+	output_dict = {key: np.asarray(value) for (key, value) in output_dict.items()}
+	output_dir = os.path.join(results_dir, "processed")
+	os.makedirs(output_dir, exist_ok=True)
+	np.savez(os.path.join(output_dir, "times.npz"), **output_dict)
+
+
+### process symmetry generators
+
+def calculate_generators(results_dir, Js, Ls, Ns):
+	generator_types = ["spatial", "internal"]
+	output_dict = {J: {generator_type: {} for generator_type in generator_types} for J in Js}
+	for J in Js:
+		for generator_type in generator_types:
+			gens = []
+			for L in Ls:
+				for N in Ns:
+					with open(os.path.join(results_dir, J, "latent_equivariant", "L{:d}".format(L), "N{:d}".format(N), "generator_reps.json"), "r") as fp:
+						gens.append( json.load(fp)[generator_type] )
+			gens = np.array(gens)
+			output_dict[J][generator_type]["mean"] = float( gens.mean() )
+			output_dict[J][generator_type]["std"] = float( gens.std() )
+	output_dir = os.path.join(results_dir, "processed")
+	os.makedirs(output_dir, exist_ok=True)
+	with open(os.path.join(output_dir, "generators.json"), "w") as fp:
+		json.dump(output_dict, fp, indent=2)
+
+
+if __name__ == "__main__":
+	Js = ["ferromagnetic", "antiferromagnetic"]
+	Ls = [16, 32, 64, 128]
+	Ns = [16, 32, 64, 128, 256, 512, 1024, 2048]
+
+#	print("Gathering magnetizations . . . ")
+#	for J in Js:
+#		for L in Ls:
+#			gather_magnetizations("data", "results", J, L)
+
+#	print("Calculating stats . . . ")
+#	for J in Js:
+#		calculate_stats("results", J, "magnetization", 128)
+#		calculate_stats("results", J, "latent", 128, N=2048)
+#		calculate_stats("results", J, "latent_equivariant", 128, N=2048)
+
+#	print("Calculating critical temperatures . . . ")
+#	for J in Js:
+#		calculate_critical_temperatures("results", J, Ls, Ns)
+
+	print("Calculating execution times . . . ")
+	calculate_times("results", Js, Ls, Ns)
+
+#	print("Calculating symmetry generators . . . ")
+#	calculate_generators("results", Js, Ls, Ns)
+
+	print("Done!")
