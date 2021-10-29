dc13c8d99889b6c63f627772ddcf812aa29eb1a1
dagrawa2 <dagrawa2@vols.utk.edu>
2021-10-29 08:14:32 -0400

    Add processing for error vs time plots.

--- list_tc.py
+++ list_tc.py
@@ -1,18 +1,26 @@
 import json
 import numpy as np
 
+results_dir = "results"
 Js = ["ferromagnetic", "antiferromagnetic"]
 Ls = [16, 32, 64, 128]
 tc = 2/np.log(1+np.sqrt(2))
 
 def tc_format(mean, std):
+	if type(mean) == str:
+		return "nan"
 	m = 100*(mean/tc-1)
 	s = 100*std/tc
 	return "{:.1f} +- {:.1f}".format(m, s)
 
+def r2_format(mean, std):
+	if type(mean) == str:
+		return "nan"
+	return "{:.1f} +- {:.1f}".format(mean, std)
+
 for J in Js:
 	print("{}\n===\n".format(J))
-	with open("results/processed/{}/tc_biased.json".format(J), "r") as fp:
+	with open("{}/processed/{}/tc_biased.json".format(results_dir, J), "r") as fp:
 		data = json.load(fp)
 	for L in Ls:
 		print("L = {:d}:".format(L))
@@ -20,5 +28,21 @@ for J in Js:
 		print("M: {}".format(tc_format(D["magnetization"]["mean"], D["magnetization"]["std"])))
 		print("N, AE, GE, MS")
 		for i in range(len(D["Ns"])):
-			print("{:d}, {}, {}, {}".format(D["Ns"][i], tc_format(D["latent"]["means"][i], D["latent"]["stds"][i]), tc_format(D["latent_equivariant"]["means"][i], D["latent_equivariant"]["stds"][i]), tc_format(D["latent_multiscale"]["means"][i], D["latent_multiscale"]["stds"][i])))
+			print("{:d}, {}, {}, {}".format(D["Ns"][i], tc_format(D["latent"]["means"][i], D["latent"]["stds"][i]), tc_format(D["latent_equivariant"]["means"][i], D["latent_equivariant"]["stds"][i]), tc_format(D["latent_multiscale_4"]["means"][i], D["latent_multiscale_4"]["stds"][i])))
 		print()
+
+	print("L = infty")
+	D = data["infty_4"]
+	print("M: {}".format(tc_format(D["magnetization"]["mean"], D["magnetization"]["std"])))
+	print("N, AE, GE, MS")
+	for i in range(len(D["Ns"])):
+		print("{:d}, {}, {}, {}".format(D["Ns"][i], tc_format(D["latent"]["means"][i], D["latent"]["stds"][i]), tc_format(D["latent_equivariant"]["means"][i], D["latent_equivariant"]["stds"][i]), tc_format(D["latent_multiscale_4"]["means"][i], D["latent_multiscale_4"]["stds"][i])))
+	print()
+
+	print("L = infty (r^2 score)")
+	D = data["r2_4"]
+	print("M: {}".format(r2_format(D["magnetization"]["mean"], D["magnetization"]["std"])))
+	print("N, AE, GE, MS")
+	for i in range(len(D["Ns"])):
+		print("{:d}, {}, {}, {}".format(D["Ns"][i], r2_format(D["latent"]["means"][i], D["latent"]["stds"][i]), r2_format(D["latent_equivariant"]["means"][i], D["latent_equivariant"]["stds"][i]), r2_format(D["latent_multiscale_4"]["means"][i], D["latent_multiscale_4"]["stds"][i])))
+	print()
--- process.py
+++ process.py
@@ -2,6 +2,7 @@ import os
 import json
 import numpy as np
 from scipy.interpolate import CubicSpline
+from scipy.linalg import lstsq
 
 from phasefinder import jackknife
 from phasefinder.datasets import Ising
@@ -97,10 +98,27 @@ def critical_temperature_samples(temperatures, u4samples, method="max_derivative
 	if method == "max_derivative":
 		return critical_temperature_samples_max_derivative(temperatures, u4samples)
 	if method == "step_function":
-		return critical_temperature_samples_max_derivative_step_function(temperatures, u4samples)
+		return critical_temperature_samples_step_function(temperatures, u4samples)
 
 
-def calculate_critical_temperatures(results_dir, J, Ls, Ns, encoder_names, remove_bias=True, method="max_derivative"):
+def lstsq_samples(x, y_samples, weights=None):
+	ones = np.ones((x.shape[0], 1))
+	if weights is None:
+		weights = ones
+	if weights.ndim == 1:
+		weights = weights[:,None]
+	if x.ndim == 1:
+		x = x[:,None]
+	x = np.concatenate((ones, x), 1)
+	fit_samples = lstsq(x*weights, y_samples*weights)[0]
+	yhat_samples = x.dot(fit_samples)
+	y_samples_centered = y_samples - y_samples.mean(0, keepdims=True)
+	yhat_samples_centered = yhat_samples - yhat_samples.mean(0, keepdims=True)
+	r2_samples = (y_samples_centered*yhat_samples_centered).sum(0)**2/( np.sum(y_samples_centered**2, 0)*np.sum(yhat_samples_centered**2, 0) )
+	return fit_samples, r2_samples
+
+
+def calculate_critical_temperatures(results_dir, J, Ls, Ns, encoder_names, remove_bias=True, method="max_derivative", jitter=1e-9):
 	output_dict = {"L{:d}".format(L): {"Ns": Ns, "magnetization": {}} for L in Ls}
 	for L in Ls:
 		for name in encoder_names:
@@ -110,19 +128,62 @@ def calculate_critical_temperatures(results_dir, J, Ls, Ns, encoder_names, remov
 		u4samples = U4_samples(results_dir, J, "magnetization", L)
 		tc_samples = critical_temperature_samples(temperatures, u4samples, method=method)
 		tc_mean, tc_std = jackknife.calculate_mean_std(tc_samples, remove_bias=remove_bias)
+		output_dict["L{:d}".format(L)]["magnetization"]["samples"] = tc_samples.tolist()
 		output_dict["L{:d}".format(L)]["magnetization"]["mean"] = float(tc_mean)
 		output_dict["L{:d}".format(L)]["magnetization"]["std"] = float(tc_std)
 		for name in encoder_names:
-			for stats in ["means", "stds"]:
+			for stats in ["samples", "means", "stds"]:
 				output_dict["L{:d}".format(L)][name][stats] = []
 		for N in Ns:
 			for name in encoder_names:
+				if not os.path.exists(os.path.join(results_dir, J, name, "L{:d}".format(L), "N{:d}".format(N))):
+					output_dict["L{:d}".format(L)][name]["samples"].append("nan")
+					output_dict["L{:d}".format(L)][name]["means"].append("nan")
+					output_dict["L{:d}".format(L)][name]["stds"].append("nan")
+					continue
 				temperatures = np.load(os.path.join(results_dir, J, name, "L{:d}".format(L), "N{:d}".format(N), "measurements.npz"))["temperatures"]
 				u4samples = U4_samples(results_dir, J, name, L, N)
 				tc_samples = critical_temperature_samples(temperatures, u4samples, method=method)
 				tc_mean, tc_std = jackknife.calculate_mean_std(tc_samples, remove_bias=remove_bias)
+				output_dict["L{:d}".format(L)][name]["samples"].append( tc_samples.tolist() )
 				output_dict["L{:d}".format(L)][name]["means"].append( float(tc_mean) )
 				output_dict["L{:d}".format(L)][name]["stds"].append( float(tc_std) )
+	def np2py(*args):
+		if type(args[0]) == np.ndarray:
+			return tuple([arg.tolist() for arg in args])
+		return tuple([float(arg) for arg in args])
+	for n_Ls in range(2, len(Ls)+1):
+		Ls_partial = Ls[:n_Ls]
+		x = 1/np.array(Ls_partial)
+		y_samples = np.array([output_dict["L{:d}".format(L)]["magnetization"]["samples"] for L in Ls_partial])
+		weights = 1/(np.array([output_dict["L{:d}".format(L)]["magnetization"]["std"] for L in Ls_partial]) + jitter)
+		fit_samples, r2_samples = lstsq_samples(x, y_samples, weights=weights)
+		samples = {"infty": fit_samples[0].T, "fit": fit_samples.T, "r2": r2_samples}
+		for (key, value) in samples.items():
+			key_n_Ls = "{}_{:d}".format(key, n_Ls)
+			mean, std = np2py( *jackknife.calculate_mean_std(value) )
+			output_dict[key_n_Ls] = {"Ns": Ns, "magnetization": {"mean": mean, "std": std}}
+			for name in encoder_names:
+				output_dict[key_n_Ls][name] = {"means": [], "stds": []}
+		for (i, N) in enumerate(Ns):
+			for name in encoder_names:
+				if "nan" in [output_dict["L{:d}".format(L)][name]["samples"][i] for L in Ls_partial]:
+					for key in ["infty", "fit", "r2"]:
+						output_dict["{}_{:d}".format(key, n_Ls)][name]["means"].append("nan")
+						output_dict["{}_{:d}".format(key, n_Ls)][name]["stds"].append("nan")
+					continue
+				y_samples = np.array([output_dict["L{:d}".format(L)][name]["samples"][i] for L in Ls_partial])
+				weights = 1/(np.array([output_dict["L{:d}".format(L)][name]["stds"][i] for L in Ls_partial]) + jitter)
+				fit_samples, r2_samples = lstsq_samples(x, y_samples, weights=weights)
+				samples = {"infty": fit_samples[0].T, "fit": fit_samples.T, "r2": r2_samples}
+				for (key, value) in samples.items():
+					key_n_Ls = "{}_{:d}".format(key, n_Ls)
+					mean, std = np2py( *jackknife.calculate_mean_std(value) )
+					output_dict[key_n_Ls][name]["means"].append(mean)
+					output_dict[key_n_Ls][name]["stds"].append(std)
+	for L in Ls:
+		for name in ["magnetization"] + encoder_names:
+			del output_dict["L{:d}".format(L)][name]["samples"]
 	output_dir = os.path.join(results_dir, "processed", J)
 	os.makedirs(output_dir, exist_ok=True)
 	suffix = "_biased" if not remove_bias else ""
@@ -154,7 +215,7 @@ def calculate_critical_temperatures_cross_lattice_sizes(results_dir, J, Ls, Ns, 
 		json.dump(output_dict, fp, indent=2)
 
 
-### process execution times
+### process execution times and error vs time trends
 
 def calculate_times(results_dir, Js, Ls, Ns, encoder_names):
 	output_dict = {"Ls": Ls, "preprocessing_means": [], "preprocessing_stds": []}
@@ -188,6 +249,71 @@ def calculate_times(results_dir, Js, Ls, Ns, encoder_names):
 	np.savez(os.path.join(output_dir, "times.npz"), **output_dict)
 
 
+def calculate_error_vs_time(results_dir, J, Ls, N, observable_names, preprocess_bools, remove_bias=True):
+	output_dict = {name: {"times_excluding_training": [], "times": [], "errors": []} for name in observable_names}
+	tc = 2/np.log(1+np.sqrt(2))
+	for L in Ls:
+		data_times_temp = []
+		preprocess_times_temp = []
+		data_dir = os.path.join("data", J, "L{:d}".format(L))
+		for dir in sorted(os.listdir(data_dir)):
+			with open(os.path.join(data_dir, dir, "time.txt"), "r") as fp:
+				data_times_temp.append( float(fp.read()) )
+			with open(os.path.join(data_dir, dir, "time_symmetric.txt"), "r") as fp:
+				preprocess_times_temp.append( float(fp.read()) )
+		data_time = sum(data_times_temp)
+		preprocess_time = sum(preprocess_times_temp)
+		for (name, preprocess) in zip(observable_names, preprocess_bools):
+			output_dict[name]["times_excluding_training"].append(data_time)
+			if preprocess:
+				output_dict[name]["times_excluding_training"][-1] += preprocess_time
+			if name == "magnetization":
+				output_dict[name]["times"].append( output_dict[name]["times_excluding_training"][-1] )
+			else:
+				with open(os.path.join(results_dir, J, name, "L{:d}".format(L), "N{:d}".format(N), "results.json"), "r") as fp:
+					output_dict[name]["times"].append( json.load(fp)["time"] + output_dict[name]["times_excluding_training"][-1] )
+			suffix = "" if remove_bias else "_biased"
+			with open(os.path.join(results_dir, "processed", J, "tc{}.json".format(suffix)), "r") as fp:
+				if name == "magnetization":
+					tc_estimate = json.load(fp)["L{:d}".format(L)][name]["mean"]
+				else:
+					tc_data = json.load(fp)["L{:d}".format(L)]
+					tc_estimate = tc_data[name]["means"][tc_data["Ns"].index(N)]
+			output_dict[name]["errors"].append( 100*(tc_estimate/tc-1) )
+	output_dir = os.path.join(results_dir, "processed", J, "N{:d}".format(N))
+	os.makedirs(output_dir, exist_ok=True)
+	with open(os.path.join(output_dir, "error_vs_time.json"), "w") as fp:
+		json.dump(output_dict, fp, indent=2)
+
+
+def calculate_error_vs_time_infty(results_dir, J, Ls, N, observable_names, multiscale_bools, remove_bias=True):
+	output_dict = {name: {"times": [], "errors": []} for name in observable_names}
+	tc = 2/np.log(1+np.sqrt(2))
+	suffix = "" if remove_bias else "_biased"
+	with open(os.path.join(results_dir, "processed", J, "tc{}.json".format(suffix)), "r") as fp:
+		tc_dict = json.load(fp)
+	with open(os.path.join(results_dir, "processed", J, "N{:d}".format(N), "error_vs_time.json"), "r") as fp:
+		errortime_dict = json.load(fp)
+	for n_Ls in range(2, len(Ls)+1):
+		Ls_partial = Ls[:n_Ls]
+		for (name, multiscale) in zip(observable_names, multiscale_bools):
+			if multiscale:
+				with open(os.path.join(results_dir, J, name, "L{:d}".format(Ls[0]), "N{:d}".format(N), "results.json"), "r") as fp:
+					output_dict[name]["times"].append( sum(errortime_dict[name]["times_excluding_training"][:n_Ls]) + json.load(fp)["time"] )
+			else:
+				output_dict[name]["times"].append( sum(errortime_dict[name]["times"][:n_Ls]) )
+			if name == "magnetization":
+				output_dict[name]["errors"].append( tc_dict["infty_{:d}".format(n_Ls)][name]["mean"] )
+			else:
+				i = tc_dict["infty_{:d}".format(n_Ls)]["Ns"].index(N)
+				output_dict[name]["errors"].append( tc_dict["infty_{:d}".format(n_Ls)][name]["means"][i] )
+			output_dict[name]["errors"][-1] = 100*(output_dict[name]["errors"][-1]/tc-1)
+	output_dir = os.path.join(results_dir, "processed", J, "N{:d}".format(N))
+	os.makedirs(output_dir, exist_ok=True)
+	with open(os.path.join(output_dir, "error_vs_time_infty.json"), "w") as fp:
+		json.dump(output_dict, fp, indent=2)
+
+
 ### process symmetry generators
 
 def calculate_generators(results_dir, Js, Ls, Ns, encoder_names):
@@ -212,35 +338,42 @@ def calculate_generators(results_dir, Js, Ls, Ns, encoder_names):
 
 
 if __name__ == "__main__":
+	results_dir = "results"
 	Js = ["ferromagnetic", "antiferromagnetic"]
 	Ls = [16, 32, 64, 128]
-	Ns = [8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-	encoder_names = ["latent", "latent_equivariant", "latent_multiscale"]
+	Ns = [2, 4, 8, 16, 32, 64, 128, 256]
+	encoder_names = ["latent", "latent_equivariant", "latent_multiscale_2", "latent_multiscale_4"]
 
 #	print("Gathering magnetizations . . . ")
 #	for J in Js:
 #		for L in Ls:
-#			gather_magnetizations("data", "results", J, L)
+#			gather_magnetizations("data", results_dir, J, L)
 
 	print("Calculating stats . . . ")
 	for J in Js:
-		calculate_stats("results", J, "magnetization", 128)
+		calculate_stats(results_dir, J, "magnetization", 128)
 		for name in encoder_names:
-			calculate_stats("results", J, name, 128, N=2048)
+			calculate_stats(results_dir, J, name, 128, N=128)
 
 	print("Calculating critical temperatures . . . ")
 	for J in Js:
 		print("J:", J)
-		for remove_bias in [True, False]:
+		for remove_bias in [False]:  # , True]:
 			print("remove bias:", remove_bias)
-			calculate_critical_temperatures("results", J, Ls, Ns, encoder_names, remove_bias=remove_bias, method="max_derivative")
-			calculate_critical_temperatures_cross_lattice_sizes("results", J, Ls, Ns, "latent_equivariant", remove_bias=remove_bias, method="max_derivative")
+			calculate_critical_temperatures(results_dir, J, Ls, Ns, encoder_names, remove_bias=remove_bias, method="step_function")
+#			calculate_critical_temperatures_cross_lattice_sizes(results_dir, J, Ls, Ns, "latent_equivariant", remove_bias=remove_bias, method="max_derivative")
 
 	print("Calculating execution times . . . ")
-	calculate_times("results", Js, Ls, Ns, encoder_names)
+	calculate_times(results_dir, Js, Ls, Ns, ["latent", "latent_equivariant"])
+
+	print("Calculating error vs time . . . ")
+	observable_names = ["magnetization"] + encoder_names
+	for J in Js:
+		calculate_error_vs_time(results_dir, J, Ls, 128, observable_names, [False, False, True, True, True], remove_bias=False)
+		calculate_error_vs_time_infty(results_dir, J, Ls, 128, observable_names, [False, False, False, True, True], remove_bias=False)
 
 	print("Calculating symmetry generators . . . ")
-	GE_encoder_names = ["latent_equivariant", "latent_multiscale"]
-	calculate_generators("results", Js, Ls, Ns, GE_encoder_names)
+	GE_encoder_names = ["latent_equivariant", "latent_multiscale_4"]
+	calculate_generators(results_dir, Js, Ls, [N for N in Ns if N >= 8], GE_encoder_names)
 
 	print("Done!")
